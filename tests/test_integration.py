f997f2d79cd12b30b83ab50d56339b7156c2c101
sametz <sametz@udel.edu>
2020-02-25 09:29:13 -0500

    Added tests for calctype, multplet and dnmr button menus. TODO: test nspins/entry widgets/j popup

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,7 +7,9 @@ from qt_nmr.controller.controller import Controller
 from qt_nmr.model.model import Model
 from tests.accepted_data.utils import load_lineshape
 
-
+# There's a lot of repetition in the test code, but trying to reduce repetition
+# by e.g. using pytest fixtures runs into problems with trying to create
+# more than one Singleton.
 
 # class App(QApplication):
 #     def __init__(self, sys_argv):
@@ -31,11 +33,37 @@ from tests.accepted_data.utils import load_lineshape
 #         assert 1 == 1
 
 
+def view_buttons(view):
+    buttons = {
+        'calctype': {
+            'multiplet': view._ui.calctype.multiplet_button,
+            'abc': view._ui.calctype.abc_button,
+            'dnmr': view._ui.calctype.dnmr_button
+        },
+        'multiplet': {
+            'AB': view._ui.multiplet_menu.AB_button,
+            'AB2': view._ui.multiplet_menu.AB2_button,
+            'ABX': view._ui.multiplet_menu.ABX_button,
+            'ABX3': view._ui.multiplet_menu.ABX3_button,
+            'AAXX': view._ui.multiplet_menu.AAXX_button,
+            '1stOrd': view._ui.multiplet_menu.firstorder_button,
+            'AABB': view._ui.multiplet_menu.AABB_button
+        },
+        'nspin': view._ui.abc_menu.buttons,
+        'dnmr': {
+            'dnmr_two_singlets': view._ui.dnmr_menu.dnmr_twospin_button,
+            'dnmr_ab': view._ui.dnmr_menu.dnmr_ab_button
+        }
+    }
+    return buttons
+
+
 class TestApp:
     def mvc(self):
         model = Model()
         controller = Controller(model)
         view = controller.view
+        view.show()
         return model, view, controller
 
     def test_instantiation(self, qtbot):
@@ -43,7 +71,7 @@ class TestApp:
         # controller = Controller(model)
         # view = controller.view
         model, view, controller = self.mvc()
-        view.show()
+        # view.show()
         qtbot.addWidget(view)
         dataitem = view._ui.plot.listDataItems()[0]
         view_data = dataitem.getData()
@@ -52,7 +80,7 @@ class TestApp:
 
     def test_nspins(self, qtbot):
         model, view, controller = self.mvc()
-        view.show()
+        # view.show()
         qtbot.addWidget(view)
         abc_button = view._ui.calctype.abc_button
         assert abc_button
@@ -62,7 +90,45 @@ class TestApp:
         expected_data = load_lineshape('nspin_2.json')
         np.testing.assert_array_almost_equal(view_data, expected_data)
 
+    def test_stack_widget(self, qtbot):
+        model, view, controller = self.mvc()
+        qtbot.addWidget(view)
+        buttons = view_buttons(view)
+        model_selections = {
+            'multiplet': view._ui.multiplet_menu,
+            'abc': view._ui.abc_menu,
+            'dnmr': view._ui.dnmr_menu
+        }
+        for calctype in ['abc', 'dnmr', 'multiplet']:
+            qtbot.mouseClick(buttons['calctype'][calctype],
+                             QtCore.Qt.LeftButton)
+            stackedwidget = view._ui.stack_model_selections
+            assert stackedwidget.currentWidget() is model_selections[calctype]
 
+    def test_all_multiplets(self, qtbot):
+        model, view, controller = self.mvc()
+        qtbot.addWidget(view)
+        buttons = view_buttons(view)
+        for model, button in buttons['multiplet'].items():
+            qtbot.mouseClick(button, QtCore.Qt.LeftButton)
+            filename = f'multiplet_{model}.json'
+            dataitem = view._ui.plot.listDataItems()[0]
+            view_data = dataitem.getData()
+            expected_data = load_lineshape(filename)
+            np.testing.assert_array_almost_equal(view_data, expected_data)
+
+    def test_all_dnmr(self, qtbot):
+        model, view, controller = self.mvc()
+        qtbot.addWidget(view)
+        buttons = view_buttons(view)
+        qtbot.mouseClick(buttons['calctype']['dnmr'], QtCore.Qt.LeftButton)
+        for model, button in buttons['dnmr'].items():
+            qtbot.mouseClick(button, QtCore.Qt.LeftButton)
+            filename = f'dnmr_{model}.json'
+            dataitem = view._ui.plot.listDataItems()[0]
+            view_data = dataitem.getData()
+            expected_data = load_lineshape(filename)
+            np.testing.assert_array_almost_equal(view_data, expected_data)
 
 # def test_basic_search(qtbot, tmpdir):
 #     """
